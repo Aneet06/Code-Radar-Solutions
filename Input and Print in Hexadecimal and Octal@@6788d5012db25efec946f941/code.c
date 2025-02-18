c71ac071f7e18d23788e566cdd479a813d7836c2
Aneet06 <aneetkaur237@gmail.com>
2025-02-18 08:37:17 +0530

    Solution to Input and Print in Hexadecimal and Octal

--- Input and Print in Hexadecimal and Octal@@6788d5012db25efec946f941/code.c
+++ Input and Print in Hexadecimal and Octal@@6788d5012db25efec946f941/code.c
@@ -1,8 +1,8 @@
 // Your code here...
 #include<stdio.h>
 int main(){
-   int a;
-   scanf("%d",&a);
+   char a;
+   scanf("%c",&a);
    printf("Hexadecimal: %x\n",a);
    printf("Octal: %o\n ",a);
    return 0 ; 
